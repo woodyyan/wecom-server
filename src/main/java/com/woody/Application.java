2360175fe2e4066af38b97c02b935cf2eaa15ba4
Woody <colorguitar@hotmail.com>
2021-12-28 11:38:09 +0800

    add search tencent doc

--- src/main/java/com/woody/Application.java
+++ src/main/java/com/woody/Application.java
@@ -2,6 +2,7 @@ package com.woody;
 
 import com.qcloud.services.scf.runtime.events.APIGatewayProxyRequestEvent;
 import com.qcloud.services.scf.runtime.events.APIGatewayProxyResponseEvent;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -9,80 +10,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
+import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Application {
 
-    public Application() {
-        List<String> replyList = Arrays.asList("抱歉我现在有事不在 等一下也不想理你",
-                "您好 发送一元红包自动解锁聊天模式",
-                "您好 我现在不无聊 希望无聊时您再找我",
-                "您的小可爱正八百里加急赶往你的聊天界面",
-                "你好 我是小王 现在认我做爹你就是小王子",
-                "魔仙堡打盹中醒了回你",
-                "咕噜咕噜魔仙堡专线在为你接通",
-                "不要烦我噢 我在冒泡泡Oooooo",
-                "老爹古董店 有事请留言",
-                "俺没回就是去拔萝卜了Ooo",
-                "我不喜欢回消息 感觉我上辈子就是个免打扰",
-                "人工服务请按1",
-                "目前心动已售完 欢迎下次光临",
-                "去宇宙摘星星啦 马上回来",
-                "别找我 有事打钱",
-                "你好 我是自动回复 您的聊天对象暂时不在",
-                "您可以和我聊天 但是我也只会这一句",
-                "恭喜你解锁我这只小可爱",
-                "我买几个橘子去 你就在此地 不要走动",
-                "我去当喜之郎了 回来带太空人给你",
-                "对方正尝试与您连接 请稍等 当前进度1%",
-                "唉呀妈呀脑瓜疼 脑瓜疼 没钱交网费脑瓜疼",
-                "欢迎使用沙雕服务热线",
-                "回复技能冷却中",
-                "您的消息已送达 对方收到 就是不回",
-                "对不起 您所联系的用户太过优秀",
-                "已被腾讯删除 想了解详情请咨询10086",
-                "稍等我 待会我用方天画戟给你削苹果吃",
-                "请输入520次我爱你召唤本人",
-                "没回信息就是在放羊 一直没回就是羊丢了",
-                "你的小宝贝不在 你爹在",
-                "这边因泄露了蟹堡王的祖传秘方 海洋监管局已将她抓获 待释放之时她会主动与您联系",
-                "嗯 你继续说 我听着呢",
-                "你是夏日限定的美好，会在赏味期内回复",
-                "在赶来与你赴约的路上",
-                "喂，这里是比基尼海滩的蟹堡王餐厅，我正在煎放在超级蟹黄堡里的肉饼，有事请找章鱼哥嘟嘟嘟嘟嘟",
-                "我和xx去当太空人了，回来给你抓外星人",
-                "在学习的海洋里溺死了",
-                "我和黑山老妖去后山讨论吃唐僧的事情，有事回来再说。",
-                "你大声点！我听不见！",
-                "不回就是在峡谷 一直不回就是被峡谷埋了",
-                "不回就是在吃鸡 一直不回就是被鸡吃了",
-                "我去宇宙了 回来摘星星给你",
-                "404 NOT FOUND",
-                "在妈妈肚子里，已经迫不及待等我出生了吗？出生了本小姐会回你哒。",
-                "你好 我们老大正在拯救银河系",
-                "打完怪兽就回来 稍等一下你就会见到他了",
-                "主人说要想知道她的踪迹需要一包番茄味薯片",
-                "鱼儿们 塘主出去撒网了 回来宠幸你们",
-                "有什么事晚上再说，幼儿园还没放学",
-                "客官请稍等，主子正在路上",
-                "没回消息就是在要饭",
-                "请大喊三声美女 我会马上出现 如没反应",
-                "说明不真诚 请再喊三声 以此类推 谢谢",
-                "你的好友已下线，请先转账后联系",
-                "对不起，对方太可爱，有事情排队预约",
-                "我在晒太阳 别打扰我",
-                "有内鬼 现在不方便回复",
-                "对方已中毒 发送我爱你即可解毒",
-                "对方网络不稳定，请稍后重试",
-                "等会再下凡见见尔等凡夫俗子",
-                "闭关修炼中");
-    }
+    private static final String SCF_CN = "云函数";
+    private static final String SCF_EN = "SCF";
 
     public static void main(String[] args) throws IOException {
-        new Application().callQingYunKe("你好");
+        new Application().searchCloudDoc("你好");
+//        new Application().callQingYunKe("你好");
     }
 
     public String mainHandler(APIGatewayProxyRequestEvent req) throws IOException {
@@ -104,17 +43,48 @@ public class Application {
         Map<String, Object> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         resp.setHeaders(new com.alibaba.fastjson.JSONObject(headers));
-        String responseBody = "{\n" +
-                "    \"msgContent\": \"%s\"\n" +
-                "}";
-        resp.setBody(String.format(responseBody, callQingYunKe(msgContent)));
-        System.out.println(responseBody);
+        if (msgContent.startsWith(SCF_EN) || msgContent.startsWith(SCF_CN)) {
+            int beginIndex = msgContent.startsWith(SCF_EN) ? SCF_EN.length() : SCF_CN.length();
+            resp.setBody(searchCloudDoc(msgContent.substring(beginIndex)));
+        } else {
+            resp.setBody(callQingYunKe(msgContent));
+        }
+        System.out.println(resp.getBody());
         return resp.toString();
     }
 
     public String callQingYunKe(String msg) throws IOException {
-        String url = "http://api.qingyunke.com/api.php?key=free&appid=0&msg=" + msg;
+        String url = "http://api.qingyunke.com/api.php?key=free&appid=0&msg=" + URLEncoder.encode(msg, "UTF-8");
+        JSONObject json = get(url);
+        JSONObject result = new JSONObject();
+        result.put("msgContent", json.getString("content"));
+        return result.toString();
+    }
 
+    public String searchCloudDoc(String query) throws IOException {
+        String url = "https://cloud.tencent.com/search/ajax/searchdoc?keyword=" + URLEncoder.encode(query, "UTF-8") + "&category=%E4%BA%91%E5%87%BD%E6%95%B0&page=1&pagesize=10";
+
+        JSONObject json = get(url);
+        JSONArray jsonArray = json.getJSONObject("data").getJSONArray("dataList");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String title = jsonObject.getString("title");
+            String urlString = jsonObject.getString("url");
+            System.out.println("title:" + title + ";urlString:" + urlString);
+            stringBuilder.append(String.format("标题：%s, 链接：%s", title, urlString));
+            stringBuilder.append(System.lineSeparator());
+        }
+        System.out.println("查询结果：");
+        System.out.println(stringBuilder);
+
+        JSONObject result = new JSONObject();
+        result.put("msgContent", stringBuilder.toString());
+        System.out.println(result);
+        return result.toString();
+    }
+
+    public JSONObject get(String url) throws IOException {
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
@@ -133,7 +103,6 @@ public class Application {
         in.close();
 
         System.out.println(response);
-        JSONObject json = new JSONObject(response.toString());
-        return json.getString("content");
+        return new JSONObject(response.toString());
     }
 }
